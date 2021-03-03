8f05676fd2c2c77f73fd5be9d9da53770e7fc541
micabaptista <baptistamichael47@gmail.com>
2021-03-03 12:09:54 +0000

    test_linux

---
+++ cmake-build-debug/CMakeFiles/Sovaccines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sovaccines.dir/ads.c.o"
+  "Sovaccines"
+  "Sovaccines.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sovaccines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
