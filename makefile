54c64f1ded83854c7dbbdd548394bc68aa69504f
micabaptista <baptistamichael47@gmail.com>
2021-03-06 17:56:56 +0000

    Comecei a fazer o process.c

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 # possivel makefile inacabado
 OBJ_dir= obj
-OBJETOS = SOVACCINES.o client.o main.o memory.o process.o proxy.o server.o synchronization.o #memory-private.o
+OBJETOS = main.o #SOVACCINES.o client.o main.o memory.o process.o proxy.o server.o synchronization.o #memory-private.o
 SOVACCINES.o =
 client.o =
 main.o =
@@ -8,11 +8,16 @@ memory.o =
 process.o =
 proxy.o =
 server.o =
-synchronization.o 0
+synchronization.o =
 #memory-private.o
 CC = gcc
 
 out: $(OBJETOS)
 	$(CC) $(addprefix $(OBJ_dir)/,$(OBJETOS)) -lm -o bin/SOVACCINES
+
 %.o: src/%.c $($@)
-	$(CC) -I include -o $(OBJ_dir)/$@ -c $<	
+	$(CC) -I include -o $(OBJ_dir)/$@ -c $<
+
+clean:
+	rm -f obj/*
+	rm -f bin/SOVACCINES
--- src/process.c
+++ src/process.c
@@ -1,4 +1,42 @@
-//
-// Created by Michael Baptista on 06/03/2021.
-//
+#include "memory.h"
+#include <unistd.h>
+#include "main.h"
+#include "client.h"
+#include "proxy.h"
+#include "server.h"
 
+int launch_process(int process_id, int process_code, struct communication_buffers *buffers, struct main_data *data,
+                   struct semaphores *sems) {
+    switch (process_code) {
+        case 0:
+            fork();
+            exit(execute_client(process_code, *buffers, *data, *sems));
+            return getpid();
+            break;
+
+        case 1:
+            fork();
+            exit(execute_proxy(process_code, *buffers, *data, *sems));
+            return getpid();
+            break;
+
+        case 2:
+            fork();
+            exit(execute_server(process_code, *buffers, *data, *sems));
+            return getpid();
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+/* Função que espera que um processo termine através da função waitpid.
+* Devolve o retorno do processo, se este tiver terminado normalmente.
+*/
+int wait_process(int process_id) {
+    // ?
+}
+
+#endif
